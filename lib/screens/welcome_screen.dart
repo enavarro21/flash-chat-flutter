437beaba474513e1429c84369cd0a61d5b7d4a29
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-08-06 11:23:42 -0700

    WelcomeScreen with animations completed

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,21 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
-
   static const String id = 'welcome_screen';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  // In order to use animations
+  AnimationController controller;
+  // In order to use curves, use type Animation()
+  // When using Curves, upperBound cannot be greater than 1
+  Animation animation;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create the a singleTicker based animation controller
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      // Upper & Lower Bound properties can be changed here
+      // upperBound: 1,
+    );
+
+    // Initialize animation to new curvedAnimation
+    // Parent is what the curve is applied to, and type of curve
+    // animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+
+    // Use ColorTween to animate between two Colors to build a new animation
+    animation = ColorTween(begin: Colors.grey, end: Colors.white).animate(controller);
+
+    // Get the animation and get it to start
+    //Proceed the animation forward
+    controller.forward();
+
+    // Get the animation to reverse, must provide a from: property
+    // from: is the new starting point
+    // controller.reverse(from: 1.0);
+
+    // Get the animation to loop, we need to know when the animation is done
+    // End of Reverse animation is AnimationStatus.dismissed
+    // End of Forward animation is AnimationStatus.complete
+    // controller.forward();
+    // animation.addStatusListener((status) {
+    //   // Can now use .dismissed and .complete to check if animation is done
+
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    // Always @override the dispose() method when the screen leaves so resources
+    // are not taken up by the application
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
+
+    // To see what the controller is doing, add a listener that takes a callback
+    controller.addListener(() {
+      // Must call setState so Flutter knows to update the screen
+      setState(() {});
+      // print(controller.value);
+      print(animation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // controller.value can be used, it is a a number between 0-1 in this case
+      // to specify the opacity of the background
+      // backgroundColor: Colors.red.withOpacity(controller.value),
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -24,14 +94,22 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    // Can even use controller.value to animate the logo height
+                    // height: animation.value * 100,
+                    height: 60,
+                  ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
+                TypewriterAnimatedTextKit(
+                  // Animate the text with the controller.value 0-100
+                  // Loading Indicator
+                  // '${controller.value.toInt()}%',
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 40.0,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
