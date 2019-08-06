51dbe0fdb3567da83123852c7a529f0cbc09a3d6
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-08-05 21:07:28 -0700

    Initial named routes for screens completed

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static const String id = 'registration_id';
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
