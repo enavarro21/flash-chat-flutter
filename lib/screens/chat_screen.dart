7d570b1987522ec07f9dd843a6875c0b1095032c
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-08-07 14:33:04 -0700

    Flash app main portion completed

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flash_chat/constants.dart';
 
+// Create instance of cloud Firestore
+final _firestore = Firestore.instance;
+// Allow access to loggedInUser in file
+FirebaseUser loggedInUser;
+
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
   @override
@@ -11,11 +16,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  // Create instance of cloud Firestore
-  final _firestore = Firestore.instance;
-
   final _auth = FirebaseAuth.instance;
-  FirebaseUser loggedInUser;
+
+  // Create a Text Editing Controller for clearing text
+  final messageTextController = TextEditingController();
 
   // Saved text from user
   String messageText;
@@ -49,15 +53,15 @@ class _ChatScreenState extends State<ChatScreen> {
   //   }
   // }
 
-  // Used to listen to a stream of data when database is updated
-  void messagesStream() async {
-    // Subscribes and listens to the snapshots streams
-    await for (var snapshot in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshot.documents) {
-        print(message.data);
-      }
-    }
-  }
+  // // Used to listen to a stream of data when database is updated
+  // void messagesStream() async {
+  //   // Subscribes and listens to the snapshots streams
+  //   await for (var snapshot in _firestore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.documents) {
+  //       print(message.data);
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -83,39 +87,7 @@ class _ChatScreenState extends State<ChatScreen> {
           children: <Widget>[
             // Show messages from database using StreamBuilder
             // Returns a list of Text Widgets.
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              // Widget that builder returns is the Column of Text Widgets
-              builder: (context, snapshot) {
-                // snapshot == Flutter's async snapshot(contains QuerySnapshot)
-                // If no Data in chat yet, display indicator
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.lightBlueAccent,
-                    ),
-                  );
-                }
-                // This snapshot now contains Firebase's QuerySnapshot, which
-                // in turn contains a list of document snapshots
-                final messages = snapshot.data.documents;
-                // Build a List of Text Widgets
-                List<Text> messageWidgets = [];
-                for (var message in messages) {
-                  // message is a Document snapshot from Firebase
-                  final messageText = message.data['text'];
-                  final messageSender = message.data['sender'];
-                  // Create message Widget
-                  final messageWidget =
-                      Text('$messageText from $messageSender');
-                  // Add messageWidget to the list of messageWidgets
-                  messageWidgets.add(messageWidget);
-                }
-                return Column(
-                  children: messageWidgets,
-                );
-              },
-            ),
+            MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -123,6 +95,8 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      // Can use this to control what is displayed in Text field
+                      controller: messageTextController,
                       onChanged: (value) {
                         //Do something with the user input.
                         messageText = value;
@@ -132,6 +106,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      // Can tell the TextEditingController to clear field
+                      messageTextController.clear();
                       //Implement send functionality.
                       //Using loggedInUser + messageText
                       // .add method expects a map{} datatype
@@ -151,6 +127,111 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      // Widget that builder returns is the Column of Text Widgets
+      builder: (context, snapshot) {
+        // snapshot == Flutter's async snapshot(contains QuerySnapshot)
+        // If no Data in chat yet, display indicator
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        // This snapshot now contains Firebase's QuerySnapshot, which
+        // in turn contains a list of document snapshots
+        final messages = snapshot.data.documents.reversed;
+        // Build a List of Text Widgets
+        List<MessageBubble> messageBubbles = [];
+        for (var message in messages) {
+          // message is a Document snapshot from Firebase
+          final messageText = message.data['text'];
+          final messageSender = message.data['sender'];
+
+          // Alternate display of the loggedInUser's message
+          final currentUser = loggedInUser.email;
+
+          // Create message Widget
+          final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              // True/False depending on if it is the currentUser
+              isMe: currentUser == messageSender);
+          // Add messageWidget to the list of messageWidgets
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 20.0,
+            ),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text, this.isMe});
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(sender,
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.black54,
+              )),
+          Material(
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 20.0,
+              ),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
